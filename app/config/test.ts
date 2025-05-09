f6f9d19d99601e6d7bf22f930c3ffa7befbb3258
Amaury PICHAT <amaury.pichat@gmail.com>
2025-05-09 08:58:19 +0200

    Create new configuration file: test.ts

---
+++ app/config/test.ts
@@ -0,0 +1,12 @@
+export const socialNetworks = {
+ 
+  "test": {
+    "name": "instagram",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"
+  },
+
+  "mastodon": {
+    "name": "mastodon",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/7/7a/Bluesky_Logo.svg"
+  }
+};
